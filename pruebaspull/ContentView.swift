b759865f469951ca16bf5f2ad8b307e77ecbf68c
JesusOrtega07 <jesus.ortega07@unach.mx>
2024-10-08 14:22:49 -0600

    Initial commit

---
+++ pruebaspull/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pruebaspull
+//
+//  Created by Jesus Ortega on 08/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pruebaspull/pruebaspullApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaspullApp.swift
+//  pruebaspull
+//
+//  Created by Jesus Ortega on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct pruebaspullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
